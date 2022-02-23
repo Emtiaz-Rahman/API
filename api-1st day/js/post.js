7771c14b9de8e4fb54c6ba3185e96fcbf2ad0fb9
Emtiaz-Rahman <emtiazrahman002@gmail.com>
2022-02-23 10:45:09 +0600

    updated

---
+++ api-1st day/js/post.js
@@ -0,0 +1,51 @@
+function loadpost(){
+    fetch('https://jsonplaceholder.typicode.com/posts')
+    .then(Response=>Response.json())
+    .then(data=>displayPost(data));
+}
+loadpost();
+
+
+
+function displayPost(posts){
+    const postContainer=document.getElementById('posts');
+    for (const post of posts){
+        const div=document.createElement('div')
+        div.classList.add('post')
+        div.innerHTML=`
+        <h3>${post.title}
+        <p>${post.body}
+        
+        `;
+        postContainer.appendChild(div);
+    }
+}
+
+
+
+
+// function loadPost() {
+//     fetch('https://jsonplaceholder.typicode.com/posts')
+//         .then(Response => Response.json())
+//         .then(data => displayPost(data));
+// }
+// loadPost();
+// function displayPost(posts) {
+//     const postContainer = document.getElementById('posts')
+//     for (const post of posts) {
+//         const div = document.createElement('div')
+//         div.classList.add('post');
+//         div.innerHTML = `
+//         <h3>${post.title}</h3>
+//         <p>${post.body}</P>
+    
+//         `;
+//         postContainer.appendChild(div);
+//     }
+
+// }
+
+
+
+const stuInfo = JSON.stringify({ name: "James", roll: 3 }); 
+console.log(stuInfo.name); 
