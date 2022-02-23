f468a1c9e2aeee0ac6753ea33d5dcba52475674d
Emtiaz-Rahman <emtiazrahman002@gmail.com>
2022-02-23 21:01:44 +0600

    picture

--- api-example/js/buddy.js
+++ api-example/js/buddy.js
@@ -1,37 +1,37 @@
 
-const loadBuddy = () => {
-    fetch('https://randomuser.me/api/?results=5')
-        .then(Response => Response.json())
-        .then(data => displayBuddy(data))
-}
-loadBuddy();
-
-const displayBuddy = data => {
-    const buddies = data.results;
-    const buddyDiv = document.getElementById('buddies');
-    for (const buddy of buddies) {
-        const p = document.createElement('p')
-        p.innerText = `Name:${buddy.name.title} ${buddy.name.first} ${buddy.name.last}  Email:${buddy.email}`
-        buddyDiv.appendChild(p);
-    }
-
-}
-
-
 // const loadBuddy = () => {
 //     fetch('https://randomuser.me/api/?results=5')
 //         .then(Response => Response.json())
-//         .then(data => displayBuddy(data));
+//         .then(data => displayBuddy(data))
 // }
 // loadBuddy();
 
 // const displayBuddy = data => {
-//     const buddies = data.results
-//     const buddyDiv = document.getElementById('buddies')
+//     const buddies = data.results;
+//     const buddyDiv = document.getElementById('buddies');
 //     for (const buddy of buddies) {
-//         console.log(buddy);
 //         const p = document.createElement('p')
-//         p.innerText = `Name:${buddy.name.title}  ${buddy.name.first} ${buddy.name.last}`;
+//         p.innerText = `Name:${buddy.name.title} ${buddy.name.first} ${buddy.name.last}  Email:${buddy.email} Gender:${buddy.gender}`
 //         buddyDiv.appendChild(p);
 //     }
+
 // }
+
+
+const loadPasswoed = () => {
+    fetch('https://randomuser.me/api/?password=upper,lower,1-16')
+        .then(Response => Response.json())
+        .then(data => displayPassword(data));
+}
+loadPasswoed();
+
+const displayPassword = password => {
+    const passwords = password.results[0];
+    console.log(passwords.picture.large);
+    const imgDiv = document.getElementById('img')
+    const div = document.createElement('div');
+    div.innerHTML = `
+    <img src="${passwords.picture.medium}" alt="">
+    `
+    imgDiv.appendChild(div);
+}
