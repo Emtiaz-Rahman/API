46e5c27bb29782dc05c1fac72a1056cfbc62f3a1
Emtiaz-Rahman <emtiazrahman002@gmail.com>
2022-02-24 12:17:27 +0600

    updated

--- api-example/js/countries.js
+++ api-example/js/countries.js
@@ -5,29 +5,37 @@ const loadCountries = () => {
 }
 loadCountries();
 const displayCountries = countries => {
-    // for (const country of countries) {
-    //     console.log(country.name);
-    // }
 
     countries.forEach(country => {
-        console.log(country);
+        // console.log(country);
         const countriesDiv = document.getElementById('countries')
         const div = document.createElement('div');
         div.classList.add('country');
         div.innerHTML = `
-       <div> <img width='300px' src="${country.flags.svg}" alt=""></div>
+       <div> <img width='200px' src="${country.flags.svg}" alt=""></div>
        <h3>Name:${country.name}</h3>
-       <p>Capital:${country.capital}
-       <p>Native Name:${country.nativeName}
+       <p>Capital:${country.capital}</P>
+       <p>Native Name:${country.nativeName}</P>
+       <button onclick="loadCountryByname('${country.name}')">Details</button>
         
         `
-
-        // const h3 = document.createElement('h3')
-        // h3.innerText = country.name
-        // div.appendChild(h3)
-        // const p = document.createElement('p')
-        // p.innerText = country.capital
-        // div.appendChild(p)
         countriesDiv.appendChild(div);
     })
+}
+const loadCountryByname = name => {
+    const url = `https://restcountries.com/v2/name/${name}`
+    fetch(url)
+        .then(Response => Response.json())
+        .then(data => displayCountryDetail(data[0]))
+}
+const displayCountryDetail = country => {
+    console.log(country)
+    const countryDiv = document.getElementById('country-detail')
+    countryDiv.innerHTML = `
+    <h3>Name:${country.name}</h3>
+       <p>Capital:${country.capital}</P>
+       <p>Native Name:${country.nativeName}</P>
+    
+    `
+
 }
